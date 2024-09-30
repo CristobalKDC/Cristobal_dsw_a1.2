56484cc6fa3cef72c34596e4f04322ca7485d15a
CristobalKDC <cristobal-pc@hotmail.com>
2024-09-30 09:40:05 +0000

    Version 1

---
+++ Enviado.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Duda enviada</title>
+</head>
+<body>
+
+    <h1>Su duda ha sido enviada con exito.</h1>
+
+    <?php
+    
+    ?>
+
+    <button type="submit" onclick="window.location.href='formulario.php';" style="background-color: blue; color: white; padding: 10px 20px; border-radius: 5px;">Enviar otra duda</button>
+
+
+</body>
+</html>
